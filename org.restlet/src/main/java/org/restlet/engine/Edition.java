6363abf6d007480fafbbbef535b08722eac6cce8
Jerome Louvel <jlouvel@restlet.com>
2016-09-17 00:36:09 -0400

    - updated copyright headers

--- org.restlet/src/main/java/org/restlet/engine/Edition.java
+++ org.restlet/src/main/java/org/restlet/engine/Edition.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2005-2014 Restlet
+ * Copyright 2005-2016 Restlet
  * <p>
  * The contents of this file are subject to the terms of one of the following
  * open source licenses: Apache 2.0 or or EPL 1.0 (the "Licenses"). You can
